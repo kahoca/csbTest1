2ad535b05ab720e3934d4c074550c66144656219
kahoca <54317572+kahoca@users.noreply.github.com>
2022-03-19 10:51:12 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h3>my CodeSandBox {c.data.header.date}</h3>
+      <h3>Date: {c.data.header.date}</h3>
+      <h3>Document no: {c.data.header.BelgeNo}</h3>
     </div>
   );
 }
